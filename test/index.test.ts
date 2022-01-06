8441f68dd19de7e2705831029dfa6cac578ff3db
kg0r0 <kg0r0@yahoo.co.jp>
2022-01-06 21:57:48 +0900

    Add test workflow

---
+++ test/index.test.ts
@@ -0,0 +1,27 @@
+const express = require('express');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+const index = require('../src/index');
+const request = require('supertest');
+
+const app = express();
+
+app.use(cookieSession({
+  keys: 'TEST_SECRET'
+}))
+
+app.use(cookieParser())
+app.use(index.TwitterOAuth2({
+  client_id: 'TEST_CLIENT_ID',
+  client_secret: 'TEST_CLIENT_SECRET',
+  redirect_uri: 'TEST_REDIRECT_URI'
+}));
+
+test('TwitterOAuth2 redirects the resource owner to twitter.', done => {
+  request(app)
+    .get('/')
+    .send()
+    .expect(
+      302, done
+    )
+})
