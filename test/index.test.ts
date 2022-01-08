ed0dd60b91a87ff7834ff3cf1ecdb50d2a53515a
kg0r0 <kg0r0@yahoo.co.jp>
2022-01-08 21:08:38 +0900

    refactoring

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,27 +1,10 @@
-import { twitterOAuth2 } from '../lib'
+import { twitterOAuth2, Options } from '../lib'
 import express from 'express';
 import session from 'express-session';
 const request = require('supertest');
 
-const app: express.Express = express();
-
-app.use(session({
-  name: 'TEST',
-  secret: 'TEST-SECRET',
-  cookie: {
-    sameSite: 'lax'
-  },
-  resave: false,
-  saveUninitialized: true
-}))
-
-app.use(twitterOAuth2({
-  client_id: 'TEST_CLIENT_ID',
-  client_secret: 'TEST_CLIENT_SECRET',
-  redirect_uri: 'TEST_REDIRECT_URI'
-}));
-
 test('TwitterOAuth2 redirects the resource owner to twitter.', done => {
+  const app = App();
   request(app)
     .get('/')
     .expect('Location', /^https:\/\/twitter.com\/i\/oauth2\/authorize.*/)
@@ -31,9 +14,29 @@ test('TwitterOAuth2 redirects the resource owner to twitter.', done => {
 })
 
 test('TwitterOAuth2 return a 403 status code when an asynchronous request is sent.', done => {
+  const app = App();
   request(app)
     .get('/')
     .set('X-Requested-With', 'XMLHttpRequest')
     .expect('Content-Type', 'application/json; charset=utf-8')
     .expect(403, done)
 })
+
+function App(twitterOAuth2Options?: Options, sessionOptions?: any) {
+  const app: express.Express = express()
+  app.use(session(sessionOptions || {
+    name: 'TEST',
+    secret: 'TEST-SECRET',
+    cookie: {
+      sameSite: 'lax'
+    },
+    resave: false,
+    saveUninitialized: true
+  }))
+  app.use(twitterOAuth2(twitterOAuth2Options || {
+    client_id: 'TEST_CLIENT_ID',
+    client_secret: 'TEST_CLIENT_SECRET',
+    redirect_uri: 'TEST_REDIRECT_URI'
+  }));
+  return app
+}
